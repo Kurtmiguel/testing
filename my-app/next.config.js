16d893a7386f28256014bdf98baece41dc01ee54
Kurtmiguel <149345822+Kurtmiguel@users.noreply.github.com>
2024-10-15 11:48:03 +0800

    Update next.config.js

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,5 +1,6 @@
-module.exports = {
-    experimental: {
-      appDir: true,
-    },
-  }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // You can add other configuration options here if needed
+};
+
+module.exports = nextConfig;
